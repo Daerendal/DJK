4df8db1717beb232afc2fa7a41f38b7403609a93
Radosław <r.jasinski@poczta.pl>
2022-03-04 09:28:07 +0100

    Add project files.

---
+++ Merkado/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Merkado.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
