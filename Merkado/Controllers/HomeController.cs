26e76bef3f04e382b51ee0e30b845fa2d9503e0a
BartekGawel99 <bartekgawel99@gmail.com>
2022-03-14 14:10:40 +0100

    add background color

--- Merkado/Controllers/HomeController.cs
+++ Merkado/Controllers/HomeController.cs
@@ -39,5 +39,10 @@ namespace Merkado.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
     }
 }
