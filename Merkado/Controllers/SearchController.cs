bffc3d4cdcea74d5a54f2c3b2e4022a70e5faf95
Daerendal <daerendal@gmail.com>
2022-03-17 21:33:03 +0100

    Add Search, SearchController

---
+++ Merkado/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using Merkado.DAL;
+using Merkado.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Merkado.Controllers
+{
+    public class SearchController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly MerkadoDbContext _db;
+
+        public SearchController(ILogger<HomeController> logger, MerkadoDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
