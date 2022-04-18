284f4aa3949c770ff5d8002ead11c71fd258ab6c
Radosław <r.jasinski@poczta.pl>
2022-04-18 12:55:59 +0200

    Edit add product page - front-end

--- Merkado/Models/Provider.cs
+++ Merkado/Models/Provider.cs
@@ -1,9 +1,14 @@
-﻿namespace Merkado.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Merkado.Models
 {
+#nullable disable
     public class Provider
     {
         public int ProviderId { get; set; }
         public string Name { get; set; }
         public List<Product> Products { get; set; }
+        [NotMapped]
+        public bool Selected { get; set; }
     }
 }
---
+++ Merkado/wwwroot/js/addProduct.js
@@ -0,0 +1,29 @@
+﻿let fileInput = document.getElementById("file-input");
+let imageContainer = document.getElementById("images");
+let numOfFiles = document.getElementById("num-of-files");
+
+function preview() {
+    if (fileInput.files.length <= 6)
+    {
+        imageContainer.innerHTML = "";
+        numOfFiles.textContent = `Liczba wybranych zdjęć: ${fileInput.files.length}`;
+        for (i of fileInput.files) {
+            let reader = new FileReader();
+            let figure = document.createElement("figure");
+            let figCap = document.createElement("figcaption");
+            figCap.innerText = i.name;
+            figure.appendChild(figCap);
+            reader.onload = () => {
+                let img = document.createElement("img");
+                img.setAttribute("src", reader.result);
+                figure.insertBefore(img, figCap);
+            }
+            imageContainer.appendChild(figure);
+            reader.readAsDataURL(i);
+        }
+    }
+    else
+    {
+        alert('Możesz dodać maksymalnie 6 zdjęć');
+    }
+}
