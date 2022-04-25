3bcee61b0d2d1e4e148ade370aaf17230f65c834
Daerendal <daerendal@gmail.com>
2022-04-25 15:19:50 +0200

    Add Search by Location (Additional Search Bar in Layout) Add currently used filters description in Search Index Add better select menu in Search Index Remove Search button in Search Index (obsolete)

--- Merkado/Controllers/SearchController.cs
+++ Merkado/Controllers/SearchController.cs
@@ -19,11 +19,13 @@ namespace Merkado.Controllers
             _db = db;
         }
       
-        public async Task<IActionResult> Index(string searchString, string currentFilter, string sortOrder, string category)
+        public async Task<IActionResult> Index(string NameSearch, string currentFilter, string sortOrder, string category, string LocationSearch)
         {
             ViewBag.CurrentSort = sortOrder;
-            ViewBag.CurrentFilter = searchString;
+            ViewBag.CurrentFilter = NameSearch;
             ViewBag.CurrentCategory = category;
+            ViewBag.CurrentLocation = LocationSearch;
+
 
             var searchResult = _db.Products
                                 .Include(c => c.Category)
@@ -33,26 +35,39 @@ namespace Merkado.Controllers
 
             
 
-            if (!String.IsNullOrEmpty(searchString))
+            if (!String.IsNullOrEmpty(NameSearch))
             {
-                searchResult = searchResult.Where(s => s.Name!.ToLower().Contains(searchString.Trim().ToLower())).ToList();
+                searchResult = searchResult.Where(s => s.Name!.ToLower().Contains(NameSearch.Trim().ToLower())).ToList();
+            }
+            if (!String.IsNullOrEmpty(LocationSearch))
+            {
+                searchResult = searchResult.Where(s => s.Localization!.ToLower().Contains(LocationSearch.Trim().ToLower())).ToList();
             }
 
-          
+
 
             switch (sortOrder)
             {
                 case "name_desc":
                     searchResult = searchResult.OrderByDescending(s => s.Name).ToList();
+                    ViewBag.CurrentSortName = "Alfabetycznie Z-A";
                     break;
-                case "Date":
+                case "date":
                     searchResult = searchResult.OrderBy(s => s.AddedDate).ToList();
+                    ViewBag.CurrentSortName = "Od Najnowszego";
                     break;
                 case "date_desc":
                     searchResult = searchResult.OrderByDescending(s => s.AddedDate).ToList();
+                    ViewBag.CurrentSortName = "Od Najstarszego";
+                    break;
+                case "name":
+                    searchResult = searchResult.OrderBy(s => s.Name).ToList();
+                    ViewBag.CurrentSortName = "Alfabetycznie A-Z";
                     break;
                 default:
                     searchResult = searchResult.OrderBy(s => s.Name).ToList();
+                    ViewBag.CurrentSortName = "Alfabetycznie A-Z";
+                    ViewBag.CurrentSort = "name";
                     break;
             }
           
