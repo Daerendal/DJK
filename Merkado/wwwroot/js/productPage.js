acd938a3f45fec024f76edf679023df5a1935593
Aleksander Jokiel <aleksander.jokiel@student.po.edu.pl>
2022-05-10 02:53:54 +0200

    Add similar items, add new features to product page

---
+++ Merkado/wwwroot/js/productPage.js
@@ -0,0 +1,13 @@
+﻿jQuery(function ($) {
+    $(".heart").on('click touchstart', function () {
+        $(this).toggleClass('is_animating');
+        $(this).toggleClass('liked');
+    });
+
+    /*when the animation is over, remove the class*/
+    $(".heart").on('animationend', function () {
+        $(this).toggleClass('is_animating');
+    });
+
+
+});
