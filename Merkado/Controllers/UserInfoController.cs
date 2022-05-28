689f05fc2e5382e6f4b648a2cc378d787c87668f
Daerendal <daerendal@gmail.com>
2022-05-28 21:44:22 +0200

    Add Basic Code for Opinions, preparing for ASP .Net implementation

--- Merkado/Controllers/UserInfoController.cs
+++ Merkado/Controllers/UserInfoController.cs
@@ -8,21 +8,29 @@ namespace Merkado.Controllers
     public class UserInfoController : Controller
     {
         private readonly MerkadoDbContext _db;
+        public static string? currentUser { get; set; }
         public UserInfoController(MerkadoDbContext db)
         {
-            _db = db;
+            _db = db;      
         }
-        
+
+       
+
         public IActionResult Index(string user, string addComment)
         {
-            if(user != null)
+            if (user != null)
+            {
+                currentUser = user;
+            }
+
+            if(currentUser != null)
             {
 
 
                 var userInfo = _db.Users
                               .Include(p => p.UserProducts)
                               .Include(o => o.Opinions)
-                              .Where(i => i.Id == user)
+                              .Where(i => i.Id == currentUser)
                               .FirstOrDefault();
 
                 var opinions = userInfo.Opinions;
@@ -32,9 +40,15 @@ namespace Merkado.Controllers
                     foreach (Opinion opinion in opinions)
                     {
                         opinion.ReviewerName = _db.Users.Where(rev => rev.Id == opinion.ReviewerId).Select(n => n.FirstName).FirstOrDefault();
+                        
                     }
 
                     userInfo.Opinions = opinions;
+                }
+                if (addComment != null)
+                {
+
+
                 }
 
 
