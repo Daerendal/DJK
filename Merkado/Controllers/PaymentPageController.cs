7f4c023f54195a766f956213d3db4b3e2724aef2
Aleksander Jokiel <aleksander.jokiel@student.po.edu.pl>
2022-06-01 19:18:14 +0200

    Try to add sending email

--- Merkado/Controllers/PaymentPageController.cs
+++ Merkado/Controllers/PaymentPageController.cs
@@ -5,6 +5,10 @@ using Merkado.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Merkado.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 
 namespace Merkado.Controllers
 {
@@ -74,11 +78,66 @@ namespace Merkado.Controllers
                 {
                     return NotFound();
                 }
+
+
+
             }
             else
             {
                 return NotFound();
             }
+        }
+        public async Task<IActionResult> SendMail(PaymentPageVM model)
+        {
+            //PIERWSZY SPOSÓB - TIME OUT
+            string to = "merkadop4d2@gmail.com"; //To address    
+            string from = "merkadop4d2@wp.pl"; //From address    
+            MailMessage message = new MailMessage(from, to);
+
+            string mailbody = "In this article you will learn how to send a email using Asp.Net & C#";
+            message.Subject = "Sending Email Using Asp.Net & C#";
+            message.Body = mailbody;
+            message.BodyEncoding = Encoding.UTF8;
+            message.IsBodyHtml = true;
+            SmtpClient client = new SmtpClient("smtp.wp.pl", 465); //Yahoo smtp    
+            System.Net.NetworkCredential basicCredential1 = new
+            System.Net.NetworkCredential("merkadop4d2@wp.pl", "P4D2Projekt123!");
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Credentials = basicCredential1;
+            try
+            {
+                client.Send(message);
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            //DRUGI SPOSÓB - NIE DZIAŁA SMTP
+            MailMessage mail = new MailMessage();
+            mail.To.Add("merkadop4d2@gmail.com");
+            mail.To.Add("aleksanderjokiel@gmail.com");
+            mail.From = new MailAddress("merkadop4d2@wp.pl");
+            mail.Subject = "Testowy mail";
+
+            string Body = "Tresc smiesznego" +
+                          "maila testowego";
+            mail.Body = Body;
+
+            mail.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.wp.pl"; //Or Your SMTP Server Address
+            smtp.Credentials = new System.Net.NetworkCredential
+                 ("merkadop4d2@wp.pl", "P4D2Projekt123!");
+            //Or your Smtp Email ID and Password
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+
+            //KONIEC 
+
+            return Redirect("Index");
         }
     }
 }
