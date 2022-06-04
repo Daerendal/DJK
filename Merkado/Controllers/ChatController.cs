e9c2a595cb8e9d5b0521c05ac1b897d045861f90
Radosław <r.jasinski@poczta.pl>
2022-06-04 19:04:39 +0200

    Add simple private chat

--- Merkado/Controllers/ChatController.cs
+++ Merkado/Controllers/ChatController.cs
@@ -1,19 +1,99 @@
 ﻿using Merkado.DAL;
+using Merkado.Models;
+using Merkado.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Merkado.Controllers
 {
     public class ChatController : Controller
     {
-        private readonly MerkadoDbContext _db;
 
-        public ChatController(MerkadoDbContext db)
+        private readonly MerkadoDbContext _db;
+        private UserManager<User> _userManager;
+
+        public ChatController(MerkadoDbContext db, UserManager<User> userManager)
         {
             _db = db;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
-            return View();
+            try
+            {
+                var loggedUser = _userManager.GetUserAsync(User).Result;
+                var myMessageSendersID = _db.ChatMessages.Where(x => x.ToUserId == loggedUser.Id).Select(s => s.FromUserId).ToList();
+                myMessageSendersID = myMessageSendersID.Distinct().ToList();
+
+                var sendersDetails = new List<User>();
+
+                foreach (var userID in myMessageSendersID)
+                {
+                    var user = _db.Users.Where(i => i.Id == userID).FirstOrDefault();
+
+                    if (user != null)
+                        sendersDetails.Add(user);
+                }
+
+                return View(sendersDetails);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
         }
+
+
+        public IActionResult Messages(string id)
+        {
+            var loggedUser = _userManager.GetUserAsync(User).Result;
+
+            var messagesFrom = _db.ChatMessages.Where(x => x.FromUserId == id && x.ToUserId == loggedUser.Id).ToList();
+            var messagesTo = _db.ChatMessages.Where(x => x.ToUserId == id && x.FromUserId == loggedUser.Id).ToList();
+
+            messagesFrom.AddRange(messagesTo);
+
+            var allMessages = messagesFrom.OrderBy(x => x.CreatedDate).ToList();
+
+            ChatVM chatVM = new ChatVM();
+            chatVM.Messages = allMessages;
+
+            if (allMessages != null)
+            {
+                return PartialView(chatVM);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        public IActionResult SendMessage(ChatVM chatVM)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(chatVM.NewMessege) && !string.IsNullOrEmpty(chatVM.ToUser))
+                {
+                    var message = new ChatMessage();
+                    message.CreatedDate = DateTime.Now;
+                    message.ToUserId = chatVM.ToUser;
+                    message.Message = chatVM.NewMessege;
+                    message.FromUserId = _userManager.GetUserAsync(User).Result.Id;
+
+                    _db.ChatMessages.Add(message);
+                    _db.SaveChanges();
+                }
+                return Redirect("Index");
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+        }
+
+
     }
 }
---
+++ Merkado/ViewModels/ChatVM.cs
@@ -0,0 +1,11 @@
+﻿using Merkado.Models;
+
+namespace Merkado.ViewModels
+{
+    public class ChatVM
+    {
+        public string NewMessege { get; set; } = string.Empty;
+        public string ToUser { get; set; }
+        public List<ChatMessage> Messages { get; set; }= new List<ChatMessage>();
+    }
+}
